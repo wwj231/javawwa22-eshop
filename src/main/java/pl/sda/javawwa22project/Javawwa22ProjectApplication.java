b0612135a619fc9958b7561e66b717bdb46b3f9a
maniek <mpastuszka@gmail.com>
2020-04-26 12:14:31 +0200

    [init] - project initial commit

---
+++ src/main/java/pl/sda/javawwa22project/Javawwa22ProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.javawwa22project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javawwa22ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javawwa22ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/javawwa22project/Javawwa22ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.javawwa22project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javawwa22ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
