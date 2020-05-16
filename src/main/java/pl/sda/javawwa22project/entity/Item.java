fc255a50bb71887c96892a6e5a0b03e61e5e9149
maniek <mariusz.pastuszka@gmail.com>
2020-05-16 10:15:51 +0200

    [change] - entity finished

--- src/main/java/pl/sda/javawwa22project/entity/Item.java
+++ src/main/java/pl/sda/javawwa22project/entity/Item.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table("SHOP_ITEMS")
+// 1). define entity
+//  - id
+// - no args constructor
 public class Item {
+    @Id
+    @GeneratedValue
     private Long id;
     private String itemName;
     private String description;
@@ -17,4 +28,5 @@ public class Item {
     private BigDecimal price;
     private int quantity;
     private String picture;
+//    private int any; // have fun:) look out!!!
 }
