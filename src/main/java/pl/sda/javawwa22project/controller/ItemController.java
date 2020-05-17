aa4c8273a7398870e38b47fde656cfe0db4b1cc8
Wladyslaw Wiktor Janicki <wwj231@gmail.com>
2020-05-17 14:06:41 +0200

    [controller] - edit form in progress

--- src/main/java/pl/sda/javawwa22project/controller/ItemController.java
+++ src/main/java/pl/sda/javawwa22project/controller/ItemController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.javawwa22project.converter.ItemConverter;
 import pl.sda.javawwa22project.dto.ItemDto;
 import pl.sda.javawwa22project.service.ItemsService;
 
+import javax.validation.Valid;
 import java.util.stream.Collectors;
 
 // 4). define endpoint in controller
@@ -51,6 +53,17 @@ public class ItemController {
 
         model.addAttribute(MANY_ITEMS_KEY, itemsToShow);
         return "items/all-items";
+    }
+
+    @GetMapping("/add-item")
+    String addItem(){
+        logger.info("addItem()");
+        return "items/add-edt";
+    }
+
+    @PostMapping("/item-save")
+    String saveItem(@Valid ItemDto itemToSave){
+        return "redirect://items/" + itemToSave.getId();
     }
     // method reference example
 //    static class Sorter {
