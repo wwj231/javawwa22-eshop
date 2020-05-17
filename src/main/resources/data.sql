a6d7f9aaf8d76c141dc0beece93cc51f73e0084c
maniek <mariusz.pastuszka@gmail.com>
2020-05-17 13:31:27 +0200

    [controller] - two endpoints allItems and oneItem workable

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into shop_items (id, category, description, item_name, picture, price, quantity)
+values (hibernate_sequence.nextval, 'covid_19', 'maseczki', 'przyłbica', null, 40.0, 5);
+
+insert into shop_items (id, category, description, item_name, picture, price, quantity)
+values (hibernate_sequence.nextval, 'covid_19', 'maseczki', 'super maseczka', null, 120.0, 5);
+
+insert into shop_items (id, category, description, item_name, picture, price, quantity)
+values (hibernate_sequence.nextval, 'covid_19', 'maseczki', 'piękna maseczka', null, 140.0, 5);
